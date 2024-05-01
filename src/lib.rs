a723cb13795c73be3a8fc5ebc887a331b1b3e1c0
zetashift <rskaraya@gmail.com>
2024-05-01 21:23:07 +0000

    resolve merge conflict

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+use zed_extension_api::{self as zed, Result};
+
+struct UnisonExtension;
+
+impl zed::Extension for UnisonExtension {
+    fn new() -> Self {
+        Self
+    }
+
+    fn language_server_command(
+        &mut self,
+        _config: &zed::LanguageServerId,
+        worktree: &zed::Worktree,
+    ) -> Result<zed::Command> {
+        let path = worktree
+            .which("ncat")
+            .ok_or_else(|| "ncat not found, please install ncat and make sure it's in your PATH".to_string())?;
+
+        Ok(zed::Command {
+            command: path,
+            args: vec!["localhost".to_string(), "5757".to_string()],
+            env: Default::default(),
+        })
+    }
+}
+
+zed::register_extension!(UnisonExtension);
